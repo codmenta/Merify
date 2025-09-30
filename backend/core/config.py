2ad344b14310c39e531ce603ecf0877f1fd0463f
codmenta <129303372+codmenta@users.noreply.github.com>
2025-09-29 20:03:32 -0500

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    ALLOWED_ORIGINS_STR: str
+    ALLOWED_ORIGINS: List[str]
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
