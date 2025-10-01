01203ce3f0caa2ad57b083a44b45675732b54a53
codmenta <129303372+codmenta@users.noreply.github.com>
2025-10-01 08:32:25 -0500

    Update products.py

--- backend/api/routes/products.py
+++ backend/api/routes/products.py
@@ -3,14 +3,6 @@ from db.json_handler import load_products
 from typing import List
 from models.order import Product
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Replace "*" with specific origins for production (e.g., ["https://your-frontend-domain.com"])
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
-)
-
 router = APIRouter()
 
 @router.get("/products", response_model=List[Product])
